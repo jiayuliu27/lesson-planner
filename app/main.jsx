33c6360c0cd8e0be082caca43af6510cc41e8aaa
jiayuliu27 <jiayuliu@umich.edu>
2017-09-24 19:53:41 -0400

    setup

---
+++ app/main.jsx
@@ -0,0 +1,15 @@
+'use strict'
+
+import React from 'react'
+import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import { Router,
+         Route,
+         IndexRoute,
+         IndexRedirect,
+         hashHistory } from 'react-router'
+
+render(
+    <div>Main.jsx</div>,
+    document.getElementById('main')
+)
